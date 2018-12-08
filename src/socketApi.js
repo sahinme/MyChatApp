9b39e4ac0c37db7db61c966c1caba2d0af24c00f
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2018-12-09 01:36:19 +0300

    socket-authorization

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,4 +1,5 @@
 const socketio=require('socket.io');
+const socketAuthorization=require('../middleware/socketAuthorization');
 
 const io=socketio();
 
@@ -6,8 +7,10 @@ const socketApi={
     io //--> ES6 io=io
 };
 
+io.use(socketAuthorization);
+
 io.on('connection',socket=>{
-    console.log("a user logged in");
+    console.log("a user logged in with this name: "+socket.request.user.name);
 });
 
 module.exports=socketApi;
